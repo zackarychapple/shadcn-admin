3a496797fd303c5391ad2ed78832bf239bab393a
shane <shane@swalker.dev>
2025-01-20 17:06:11 -0600

    feat: migrate to rsbuild

---
+++ rsbuild.config.ts
@@ -0,0 +1,13 @@
+// rsbuild.config.ts
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  tools: {
+    rspack: {
+      plugins: [TanStackRouterRspack()],
+    },
+  },
+})
